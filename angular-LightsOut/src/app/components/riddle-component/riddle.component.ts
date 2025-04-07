ed37d2ed2571cce45f732b4c49ecbf25bb73336e
angelikavolaric <volaric.angelika@gmail.com>
2025-04-07 15:25:29 +0200

    adding button

--- angular-LightsOut/src/app/components/riddle-component/riddle.component.ts
+++ angular-LightsOut/src/app/components/riddle-component/riddle.component.ts
@@ -16,58 +16,59 @@ import { error } from 'node:console';
   styleUrl: './riddle.component.css'
 })
 export class RiddleComponent {
-  title = 'LightsOut';
+  title = 'riddle LightsOut';
 
   
  // problem = Problem; //specific problem
-  problem: Problem = { _id: "33", size: 4, sequence: [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]};
-
+  problem: Problem = {id: "0", sequence: []};//{ id: "33", size: 4, sequence: [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]};
+  id: string = "0"
   problemSequence: number[] = [];
   arrayProblem: number[] = [];
   sizeOfProblem: boolean[][] = [];
   isSholutionOn: boolean = false; //If we show solution
-  solution: Solution[] = [
-    { _id: "0", fkProblem: "33"}
-  ];
-
+  solution: Solution[] = [];
+  isEnd: boolean = false;
   solutionPoints: number[] = [] //for each wanted solution the bord fills with points needed to be pressed (and number of times)
 
- /*solutionStep: SolutionStep = 
-    { _id: "0", actions: [3, 6, 10, 13, 15], actionSeq: [1, 2, 3, 4, 5], fkSolution: "0"};*/
+    solutionSteps: SolutionStep[] = []
+  routeSub: any;
 
-    solutionSteps: SolutionStep[] = [
-    { _id: "0", action: 0, actionSeq: 1, fkSolution: "0"},
-    { _id: "1", action: 1, actionSeq: 2, fkSolution: "0"},
-    { _id: "2", action: 2, actionSeq: 3, fkSolution: "0"},
-    { _id: "3", action: 7, actionSeq: 4, fkSolution: "0"} //reši 1
-    ]
-    // solutionStep[]->mult.
-
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+      private route: ActivatedRoute, 
+      private problemService: ProblemService,
+      private solutionService: SolutionService,
+    ) { }
 
   ngOnInit(): void {
 
     this.route.params.subscribe(params => {
-      let id = params['id'];  // 'id' is the parameter name in the URL
+      this.id = params['id'];  // Using id from url
     });
 
-    //this.problem = getProblem(id);
+      this.getProblem()
+  }
 
-      this.arrayProblem = this.problem.sequence;
-      this.problemSequence = this.problem.sequence
-      this.sizeOfProblem = new Array(this.problem.size).fill(null);
-      //TODO: add filling this.problems from DB
-      //console.log(this.problems[pr]._id, this.problems[pr].size)
-      //console.log(this.sizeOfProblem)
-  
-    
+
+  getProblem() {
+    this.problemService.getProblem(this.id).subscribe((pr: Problem) => {
+      this.problem = pr
+       this.processProblem()
+    },
+    (error) => {
+      console.error('Error fetching problems:', error);
+    });
+
+  }
+
+  processProblem() {
+    this.problemSequence = this.problem.sequence
+    this.sizeOfProblem = new Array(Math.sqrt(this.problem.sequence.length)).fill(null);
   }
 
   getIndex(x: number, y: number): number {
-   // console.log("indey"+ ( y + (x * this.problems[i].size)))
-   //console.log(this.chosenRiddle== y + (x * this.problems[i].size) && this.solutionPoints[  y + (x * this.problems[i].size)]!=0)
-    return y + (x * this.problem.size); // Ensure index is a number
-  }
+    let size = Math.sqrt(this.problem.sequence.length)
+    return y + (x * size); // Ensure index is a number
+   }
 
   
   problemButtonClick(): void {
@@ -76,19 +77,22 @@ export class RiddleComponent {
 
   /*we fill array with solution - only one for now!!!!!!!!!!!!!!!!!*/
   showSolution(): void {
-    /*getSolution(problemId); */
-    /*get problem problems(id) */
+    this.getProblem( )
+    let newSol = new Array(this.problem.sequence.length).fill(0);
+    this.solutionService.getSolutionSteps(this.id).subscribe((ss: SolutionStep[]) => {
+      ss.forEach(((solStep: SolutionStep) => {
+        newSol[solStep.action] += 1
 
-    let newSol = new Array(Math.pow(this.problem.size, 2)).fill(0);
-      this.solutionSteps.forEach((solStep: SolutionStep) => {
-          newSol[solStep.action] += 1
+    }))
+    },
+    (error) => {
+      console.error('Error fetching problems:', error);
+    });
+    console.log("newSol",newSol)
 
-      }); 
-       // console.log("Problem with showing solution, try later!")
-      
       this.solutionPoints=newSol
       this.isSholutionOn = true
-      this.resetButtonClick( )
+      
     
   }
 
@@ -109,22 +113,29 @@ export class RiddleComponent {
   solveButtonClick(buttonId: number): void {
   //TODO: curr for chosen problem
   let seq = this.problem.sequence
-  let size = this.problem.size
+  let size = Math.sqrt(this.problem.sequence.length)
   
-
-  if(buttonId % size == 0) { //left corner 
-    seq[buttonId + size] = this.revertValue(seq[buttonId + size]) //down
-    seq[buttonId - size] = this.revertValue(seq[buttonId - size]) //up
+  if(buttonId % size == 0) { //left corner
     seq[buttonId + 1] = this.revertValue(seq[buttonId + 1]) //right
+    if(((buttonId - (buttonId % size)) /  size) != (size - 1)){ //if not bottom
+      seq[buttonId + size] = this.revertValue(seq[buttonId + size]) //down
+    }
+    if(((buttonId - (buttonId % size)) /  size) != 0) { //if not top
+      seq[buttonId - size] = this.revertValue(seq[buttonId - size]) //up
+    }
   } else if((buttonId + 1) % size == 0){  //right corner
-    seq[buttonId + size] = this.revertValue(seq[buttonId + size]) //down
-    seq[buttonId - size] = this.revertValue(seq[buttonId - size]) //up
     seq[buttonId - 1] = this.revertValue(seq[buttonId - 1]) //left
-  } else if(buttonId / size== 1) { //top
+    if(((buttonId - (buttonId % size)) /  size) != (size - 1)){ //if not bottom
+      seq[buttonId + size] = this.revertValue(seq[buttonId + size]) //down
+    }
+    if(((buttonId - (buttonId % size)) /  size) != 0) { //if not top
+      seq[buttonId - size] = this.revertValue(seq[buttonId - size]) //up
+    }
+  } else if(((buttonId - (buttonId % size)) /  size) == 0) { //top
     seq[buttonId + size] = this.revertValue(seq[buttonId + size]) //down
     seq[buttonId - 1] = this.revertValue(seq[buttonId - 1]) //left
     seq[buttonId + 1] = this.revertValue(seq[buttonId + 1]) //right
-  } else if(buttonId / size == size) { //bottom
+  } else if(((buttonId - (buttonId % size)) /  size) == (size - 1)) { //bottom
     seq[buttonId - size] = this.revertValue(seq[buttonId - size]) //up
     seq[buttonId - 1] = this.revertValue(seq[buttonId - 1]) //left
     seq[buttonId + 1] = this.revertValue(seq[buttonId + 1]) //right
@@ -134,19 +145,14 @@ export class RiddleComponent {
     seq[buttonId - 1] = this.revertValue(seq[buttonId - 1]) //left
     seq[buttonId + 1] = this.revertValue(seq[buttonId + 1]) //right
   }
-  seq[buttonId] = this.revertValue(seq[buttonId]) 
-  //TODO: check if actualy works-> bleed from others (should work - first looks right, left)
+  seq[buttonId] = this.revertValue(seq[buttonId])  //revert current button
+
   this.problem.sequence = seq
+  this.isEnd = this.problemSequence.every(element => element === 0);
 
     return 
   }
-
-  resetButtonClick(): void {
-   // let resProblem: Problem = this.problemService.getProblem(problemId);
-   /* this.problemService.getProblem(problemId).subscribe((problem: Problem) => {
-      this.problem = problem
-    })
-    this.problems[problemIndex] = this.problem*/
-  }
+    
+    
 
 }
